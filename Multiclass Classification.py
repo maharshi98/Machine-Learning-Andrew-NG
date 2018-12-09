effe8eae89fa9964bfcf82f335f360263f386b6c
Maharshi Doshi <maharshi98@gmail.com>
2018-12-09 12:35:56 +0530

    Update Multiclass Classification.py

--- Multiclass Classification.py
+++ Multiclass Classification.py
@@ -130,8 +130,6 @@ theta = oneVsAll(X, y, 10, 0.1)
 def predictOneVsAll(all_theta, features):
     probs = sigmoid(X.dot(all_theta.T))
         
-    # Adding one because Python uses zero based indexing for the 10 columns (0-9),
-    # while the 10 classes are numbered from 1 to 10.
     return(np.argmax(probs, axis=1)+1)
 
 
@@ -145,9 +143,7 @@ print('Training set accuracy: {} %'.format(np.mean(pred == y.ravel())*100))
 # In[19]:
 
 
-#using scikit learn
 clf = LogisticRegression(C=10, penalty='l2', solver='liblinear')
-# Scikit-learn fits intercept automatically, so we exclude first column with 'ones' from X when fitting.
 clf.fit(X[:,1:],y.ravel())
 
 
